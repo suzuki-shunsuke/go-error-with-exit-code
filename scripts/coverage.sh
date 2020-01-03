c3e24a2a8fae3a04f0ab1f401b922d27eeea6c70
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-01-03 18:30:04 +0900

    ci: use shfmt and shellcheck

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,3 +1,7 @@
-mkdir -p .coverage/$1
-go test ./$1 -coverprofile=.coverage/$1/coverage.txt -covermode=atomic
-go tool cover -html=.coverage/$1/coverage.txt
+#!/usr/bin/env sh
+
+cd "$(dirname "$0")"/.. || exit 1
+
+mkdir -p ".coverage/$1" || exit 1
+go test "./$1" -coverprofile=".coverage/$1/coverage.txt" -covermode=atomic || exit 1
+go tool cover -html=".coverage/$1/coverage.txt"
